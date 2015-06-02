149a8fb2f6254558830eff15008e1b73edec31ed
E <E@Es-MacBook-Pro.local>
2015-06-01 19:51:33 -0700

    Initial Commit

---
+++ DAT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DAT
+//
+//  Created by E on 6/1/15.
+//  Copyright (c) 2015 Eyad_Neesha_Adeeb_Ehab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DATTests/DATTests.swift
@@ -0,0 +1,36 @@
+//
+//  DATTests.swift
+//  DATTests
+//
+//  Created by E on 6/1/15.
+//  Copyright (c) 2015 Eyad_Neesha_Adeeb_Ehab. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DATTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
