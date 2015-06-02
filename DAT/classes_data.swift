00180dad45277ef7a362da896d85efb55e6a0bc8
Adib Behjat <abehjat@gmail.com>
2015-06-01 21:10:30 -0700

    Completed database schema

---
+++ DAT/classes_data.swift
@@ -0,0 +1,19 @@
+//
+//  classes_data.swift
+//  DAT
+//
+//  Created by Adib Behjat on 6/1/15.
+//  Copyright (c) 2015 Eyad_Neesha_Adeeb_Ehab. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class classObject: NSManagedObject {
+    @NSManaged var igetc_area : String! // IGETC Area
+    @NSManaged var department : String! // Department ID
+    @NSManaged var course_num : String! // Course Number
+    @NSManaged var course_unt : Double  // Course Units
+    @NSManaged var course_tle : String! // Course Title
+    @NSManaged var course_des : String! // Course Description
+}
---
+++ DAT/grade_struct.swift
@@ -0,0 +1,24 @@
+//
+//  grade_struct.swift
+//  DAT
+//
+//  Created by Adib Behjat on 6/1/15.
+//  Copyright (c) 2015 Eyad_Neesha_Adeeb_Ehab. All rights reserved.
+//
+
+import Foundation
+
+struct grade {
+    static let Ap = 4.0
+    static let A  = 4.0
+    static let Am = 3.7
+    static let Bp = 3.3
+    static let B  = 3.0
+    static let Bm = 2.7
+    static let Cp = 2.3
+    static let C  = 2.0
+    static let Dp = 1.3
+    static let D  = 1.0
+    static let Dm = 0.7
+    static let F  = 0
+}
