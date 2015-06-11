3143e015cdaffb1739274fe117e33acd1a41c192
Adib Behjat <abehjat@gmail.com>
2015-06-10 21:29:27 -0700

    classObject

--- DAT/classObject.swift
+++ DAT/classObject.swift
@@ -10,15 +10,15 @@ import Foundation
 import CoreData
 
 class classObject: NSManagedObject {
-
-    @NSManaged var course_des: String
-    @NSManaged var course_grd: String
-    @NSManaged var course_num: String
-    @NSManaged var course_tkn: NSNumber
-    @NSManaged var course_tle: String
-    @NSManaged var course_unt: String
-    @NSManaged var department: String
-    @NSManaged var igetc_area: String
-    @NSManaged var igetc_suba: String
-
+    
+    @NSManaged var course_des: String //Course Description
+    @NSManaged var course_num: String //Course Number
+    @NSManaged var course_tle: String //Course Title
+    @NSManaged var course_unt: String //Course Unit
+    @NSManaged var department: String //Course Department
+    @NSManaged var igetc_area: String //Course IGETC Area
+    @NSManaged var igetc_suba: String //Course IGETC Sub Area
+    @NSManaged var course_tkn: NSNumber //Course Taken
+    @NSManaged var course_grd: String //Course Grade
+    
 }
